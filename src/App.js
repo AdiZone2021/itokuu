973f68554bc1e920bd841f23722703992e64a098
AdiZone) <86079921+AdiZone2021@users.noreply.github.com>
2021-10-26 01:48:52 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm.js';
+import ReactPlayer from 'react-player'
+
 
 function App() {
+  const adminUser = {
+    email: "Okuu@it.com",
+    password: "nomad2023"
+  }
+
+  const [user, setUser] = useState({name:"", email:""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password){
+        console.log("Logged in");
+        setUser({
+          name:details.name,
+          email:details.email,
+        });
+  } else{
+        console.log("Details do not match!");
+        setError("Маалыматтар дал келген жок!");
+    }
+  }
+  
+  
+  const Logout = () => {
+    setUser({name:"", email:"" });
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email !="")?(
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <div className="player"> 
+          <ReactPlayer with='100px' height='300px' controls url='https://youtu.be/OtJkXuqyjkk' />
+          </div>
+            <button onClick={Logout}>Logout</button>
+        </div>
+      ):(
+      <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
